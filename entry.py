9c7ea2f7d39d50b94e5abc08f2e6e6a99bcc4c3e
Shyam <shyam12860@gmail.com>
2016-12-01 00:47:55 -0500

    Hopefully it works now

--- entry.py
+++ entry.py
@@ -36,7 +36,11 @@ def echo_entities(request):
     print "======================================================="
     result = getResults(query)
     if result:
-        context['result'] = result[1]
+        context['result'] = result[1]['name'] + 'situated at' + result[1]['address']
+        context['result'] = context['result'] + '. Here\'s the url: ' + result[1]['url'] if result[1]['url'] else context['result']
+        #context['name'] = result[1]['name']
+        #context['address'] = result[1]['address']
+        #context['url'] = result[1]['url']
     return context
 
 def send(request, response):
